11c73af87b4d352121e95791d80c44c9f06b9295
kingkaminseo <136690999+kingkaminseo@users.noreply.github.com>
2024-01-16 23:57:47 +0900

    portfolio (1)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,17 @@ function Main() {
         <div style={{paddingTop : '200px', paddingBottom : '200px'}}>
         <h1 style={{color : 'white', fontSize : '100px',fontWeight : '600'}}>김민서 portFolio</h1>
         <h1 style={{color : 'white', fontSize : '100px',fontWeight : '600'}}>FrontEnd Developer</h1>
-        <div style={{width : '200px', height : '200px', borderRight : '3px solid white', borderRadius : '50%', float : 'left'}}></div>
+        <div style={{
+    width: '400px',
+    height: '400px',
+    borderRight: '2px dashed rgba(255, 255, 255, 0.8)', // 조절 가능한 투명도
+    borderRadius: '50%',
+    float: 'left',
+    position: 'absolute',
+    top: -10,
+    transform: 'rotate(40deg)',
+}}></div>
+
         
         </div>
     );
